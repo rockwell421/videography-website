1680f63097c561ca7630656ffc114d8ac2e05fff
Eddy Drew <eddy@xcap.ca>
2017-10-20 19:21:11 -0400

    backend v1

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 /*
 This file runs the website
 */
+//basic server setup
+const express = require('express');
+const knex = require('knex')({ client: 'mysql' });
+const INQUIRY = ['name', 'email', 'message', 'createdAt', 'updatedAt'];
+
+
+var http = require('http');
+
+
+var server = http.createServer(function(req,res){
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello');
+});
+
+server.listen(3000, '127.0.0.1');
+console.log('yo bitches');
